f6259f0ddf276a9601d8c6646e66447f61297a27
A.I <merrick@luois.me>
2017-05-09 16:05:19 +0800

    make things work

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true,
+    "jest": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 8,
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      "tab"
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single",
+      { "allowTemplateLiterals": true }
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "no-console": 0
+  }
+};
